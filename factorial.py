1f1541e9566cc9248f352abeebc15d469f6e67b0
Vishal Umavane <vsumavane@outlook.com>
2024-01-13 14:21:29 +0530

    Create factorial.py

---
+++ factorial.py
@@ -0,0 +1,9 @@
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result = result * i
+    return result
+
+num = 5
+fact = factorial(num)
+print(f"The factorial of {num} is: {fact}")
